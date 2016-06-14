caced8297cfeea99c168c3bffb80702d6accffa7
kjiwu <kjiwu_001@sina.com>
2016-06-14 21:36:23 +0800

    add weather activity

--- app/src/main/java/com/kjiwu/coolweather/util/Utility.java
+++ app/src/main/java/com/kjiwu/coolweather/util/Utility.java
@@ -1,11 +1,20 @@
 package com.kjiwu.coolweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.kjiwu.coolweather.db.CoolWeatherDB;
 import com.kjiwu.coolweather.model.City;
 import com.kjiwu.coolweather.model.County;
 import com.kjiwu.coolweather.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by kjiwu on 16/6/13.
@@ -70,5 +79,39 @@ public class Utility {
         }
 
         return false;
+    }
+
+    public static void handleWeatherResponse(Context context, String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context, cityName, weatherCode, temp1, temp2, weatherDesp,
+                    publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode,
+                                       String temp1, String temp2, String weatherDesp,
+                                       String publishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日");
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences
+                (context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temnp1", temp1);
+        editor.putString("temnp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
+        editor.commit();
     }
 }
